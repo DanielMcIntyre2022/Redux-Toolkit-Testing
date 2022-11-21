e1ed6f5a4e05ac3ebeef3230d43f046e05cb8321
Daniel <daniel-mcintyre@hotmail.com>
2022-11-21 15:25:52 -0500

    created increase reducer function and imported/used in CartItem component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,5 @@
 import { ChevronDown, ChevronUp } from '../icons';
-import { removeItem } from '../features/cart/cartSlice';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
 import { useDispatch} from 'react-redux';
 
 function CartItem({ id, img, title, price, amount}) {
@@ -17,7 +17,9 @@ function CartItem({ id, img, title, price, amount}) {
         }}>remove</button>
       </div>
       <div>
-        <button className='amount-btn'>
+        <button className='amount-btn' onClick={() => {
+          dispatch(increase({id}))
+        }}>
           <ChevronUp/>
         </button>
         <p className='amount'>{amount}</p>
